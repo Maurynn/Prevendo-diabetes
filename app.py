cb12cacbe4de877b08c7be49ea1ed8b6fb730f37
Mauro Alves <mauro.mn@hotmail.com>
2023-11-10 01:57:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scikitlearn.tree import DecisionTreeClassifier
-from scikitlearn.model_selection import train_test_split
-from scikitlearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scikitlearn.tree import plot_tree
+from sklearn.tree import plot_tree
 
 # Definindo o título do app
 st.title("Prevendo Diabetes")
