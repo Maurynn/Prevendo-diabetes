cbb7097835472d68b1f46e685d3d29ca0049b509
Mauro Alves <mauro.mn@hotmail.com>
2023-11-10 17:00:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,8 @@ def generate_pdf_report(paciente_nome, prediction, decision_tree_fig):
     
     c.drawString(100, 250, f"Nome do Paciente: {paciente_nome}")
     c.drawString(100, 230, f"Resultado da Previsão: {'Diabetes' if prediction == 1 else 'Sem Diabetes'}")
-    c.drawString(100, 210, "Outras informações do paciente...")
+    c.drawString(100, 210, f"Idade: {age}")
+    c.drawString(100, 190, "f"Sexo: {sex}")
 
     c.showPage()
     c.save()
