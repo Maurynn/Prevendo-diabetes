782066990d401dac1366ece1e47acd88ea4ffe28
Mauro Alves <mauro.mn@hotmail.com>
2023-11-13 14:27:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ if button:
         if user_pred[0] == 0:
             st.markdown("<h1 style='color: green;'>Parabéns! Você não tem diabetes.</h1>", unsafe_allow_html=True)
         else:
-            st.markdown("<h1 style='color: red;'>Atenção! Você tem diabetes.</h1>", unsafe_allow_html=True)
+            st.markdown("<h1 style='color: red;'>⚠️Atenção! Você tem diabetes.</h1>", unsafe_allow_html=True)
         #if user_pred[0] == 0:
             #st.success("Parabéns! Você não tem diabetes.")
         #else:
