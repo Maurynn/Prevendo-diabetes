3bada79896881271fff113b0610092d210ac724b
Mauro Alves <mauro.mn@hotmail.com>
2023-11-14 12:33:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -121,15 +121,7 @@ with tab1:
             file_name=f"Diabetes_Report_{paciente_nome}.pdf",
             help="Clique para baixar o relatório em PDF"
     
-        
-        #with open(f"Diabetes_Report_{paciente_nome}.pdf", "rb") as pdf_file:
-            #b64_pdf = base64.b64encode(pdf_file.read()).decode()
-    
-        #href = f'<a href="data:application/pdf;base64,{b64_pdf}" download="Diabetes_Report_{paciente_nome}.pdf">Baixar Relatório em PDF</a>'
-        #st.markdown(href, unsafe_allow_html=True)
-        #st.success(f"Relatório em PDF gerado com sucesso para {paciente_nome}")
-    
-    #clf = DecisionTreeClassifier(criterion="entropy", max_depth=3, random_state=42)   
+          
     # Verificando se o botão foi clicado
     if button:
         if uploaded_file is not None:
