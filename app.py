9f5ce136d91ccb725fae721b8b9585a96bc4e5d5
Mauro Alves <mauro.mn@hotmail.com>
2023-11-14 09:13:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -99,8 +99,7 @@ with tab1:
         if download_button:
             with open(f"Diabetes_Report_{paciente_nome}.pdf", "rb") as pdf_file:
                 b64_pdf = base64.b64encode(pdf_file.read()).decode()
-
-            href = f'<a href="data:application/pdf;base64,{b64_pdf}" download="Diabetes_Report_{paciente_nome}.pdf">Baixar Relatório em PDF</a>'
+            href = f'<a href="data:application/octet-stream;base64,{b64_pdf}" download="Diabetes_Report_{paciente_nome}.pdf">Baixar Relatório em PDF</a>'
             st.markdown(href, unsafe_allow_html=True)
 
             st.success(f"Relatório em PDF gerado com sucesso para {paciente_nome}")
