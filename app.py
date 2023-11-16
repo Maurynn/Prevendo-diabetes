c9d40397b648b80d2b9496633154e52e6b765015
Mauro Alves <mauro.mn@hotmail.com>
2023-11-16 20:06:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -218,3 +218,23 @@ O tratamento da diabetes visa manter a glicose no sangue dentro dos valores norm
 A prevenção da diabetes é possível, principalmente no caso do diabetes tipo 2. A prevenção envolve a adoção de hábitos de vida saudáveis, como evitar o tabagismo, o consumo excessivo de álcool, o estresse, o sedentarismo e a obesidade. A alimentação saudável é um dos pilares da prevenção, pois ajuda a controlar o peso corporal, a glicose no sangue, o colesterol e a pressão arterial. A alimentação saudável deve ser equilibrada, variada, colorida e adequada às necessidades de cada pessoa. Alguns dos alimentos que devem ser consumidos com moderação são: açúcar, doces, refrigerantes, sucos industrializados, farinha branca, pão branco, arroz branco, massas, batata, mandioca, frituras, gorduras saturadas, gorduras trans, sal e alimentos processados. Alguns dos alimentos que devem ser consumidos com frequência são: frutas, verduras, legumes, cereais integrais, leguminosas, oleaginosas, sementes, leite e derivados desnatados, carnes magras, peixes, ovos, azeite de oliva, alho, cebola, ervas e especiarias.
 
 Fontes: [Sociedade Brasileira de Diabetes](http://www2.datasus.gov.br/SIAB/index.php?area=02), [Ministério da Saúde](https://www.kaggle.com/datasets/datahackers/state-of-data-2021), [Portal Data Science](https://portaldatascience.com/kaggle/)""")
+
+# Definindo o DataFrame globalmente
+df = pd.DataFrame()
+
+with tab4:
+    st.header("Visualizar Distribuições")
+
+    # Verifica se o DataFrame foi carregado
+    if not df.empty:
+        st.write("Explore as distribuições de diferentes variáveis nos casos de diabetes e sem diabetes.")
+
+        # Selecione a variável para visualizar
+        variavel_selecionada = st.selectbox("Selecione a Variável", df.columns)
+
+        # Visualize a distribuição usando um gráfico de barras ou histograma
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.histplot(data=df, x=variavel_selecionada, hue="Outcome", kde=True, multiple="stack", ax=ax)
+        st.pyplot(fig)
+    else:
+        st.warning("Por favor, faça o upload do arquivo CSV na tab '🏠Home' para explorar as distribuições.")
